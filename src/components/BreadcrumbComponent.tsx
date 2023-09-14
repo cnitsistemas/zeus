2946607b92f4cd6fab6790db52315e68a44c0100
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2023-09-14 18:26:21 -0300

    Corrigindo Component de Breadcrumb

---
+++ src/components/BreadcrumbComponent.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Breadcrumbs, Stack } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+export default function BreadcrumbComponent({
+  breadcrumbItens,
+}: {
+  breadcrumbItens: any;
+}) {
+  return (
+    <>
+      <Stack spacing={2}>
+        <Breadcrumbs
+          sx={{ fontSize: "14px" }}
+          separator={<ArrowForwardIosIcon sx={{ fontSize: 10 }} />}
+          aria-label="breadcrumb"
+        >
+          {breadcrumbItens}
+        </Breadcrumbs>
+      </Stack>
+    </>
+  );
+}
